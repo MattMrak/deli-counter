d0a301bda2e5a05b092cc3e4bde8fd548c3090d0
MattMrak <matthewmrakovcic@gmail.com>
2020-12-11 17:36:52 -0500

    commit tree b22aa733cd6fd2cfc3950e7385f65a87c2523211

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+def take_a_number(katz_deli, name)
+    katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+    if katz_deli.length === 0
+        puts "There is nobody waiting to be served!"
+    else 
+        puts "Currently serving #{katz_deli.shift}."
+    end
+end
+
+def line(katz_deli)
+
+    
+    if katz_deli.length === 0
+        puts "The line is currently empty."
+    else
+        message = "The line is currently:"
+        katz_deli.each_with_index do |customer, number|
+            message += " #{number+1}. #{customer}"
+          
+        end
+        puts "#{message}"
+    end
+    
+end
+
+# creating a block that performs each_with_index...
+# on the katz_deli array and extracts both the...
+# element and the elements index so you can print...
+# their name and place in line based on the... 
+# element and index.
+# element = name, index = place in line
